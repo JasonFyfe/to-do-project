4d10e54f885c01f29a7c2bed39057584de337953
Jason-Fyfe <JFyfe@academytrainee.com>
2020-12-10 18:03:10 +0000

    TDP-33 TodoDTO implemtation

---
+++ src/main/java/com/qa/notes/dto/TodoDTO.java
@@ -0,0 +1,13 @@
+package com.qa.notes.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TodoDTO {
+	
+	private Long id;
+	private String title;
+
+}
